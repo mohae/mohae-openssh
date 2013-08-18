3929c10e071129730eaf86c4d00e4acc2194c9ab
Count Derp <thecount@moehae.com>
2013-08-18 13:39:19 -0500

    corrected spelling error in var

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,5 +4,5 @@ override['openssh']['server']['Protocol']             = 2
 override['openssh']['server']['PermitRootLogin']      = 'no'
 override['openssh']['server']['LoginGraceTime']       = '5s'
 override['openssh']['server']['MaxAuthTries']         = 5
-override['openssh']['server']['MaxSession']           = 4
+override['openssh']['server']['MaxSessions']           = 4
 override['openssh']['server']['PermitEmptyPasswords'] = 'no'
