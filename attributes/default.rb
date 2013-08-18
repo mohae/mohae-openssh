ff65bb317839700e13a9b7770d5150f498b2b4b5
Count Derp <thecount@moehae.com>
2013-08-18 13:47:56 -0500

    added quotes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,8 +1,8 @@
 
-default['openssh']['server']['port']                 = 1001
-default['openssh']['server']['protocol']             = 2
+default['openssh']['server']['port']                 = '1001'
+default['openssh']['server']['protocol']             = '2'
 default['openssh']['server']['login_grace_time']     = '5s'
 default['openssh']['server']['permit_root_login']      = 'no'
-default['openssh']['server']['max_auth_tries']         = 5
-default['openssh']['server']['max_sessions']           = 4
+default['openssh']['server']['max_auth_tries']         = '5'
+default['openssh']['server']['max_sessions']           = '4'
 default['openssh']['server']['permit_empty_passwords'] = 'no'
