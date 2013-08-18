7be1273476e8e25ee89839c6680863985eb564db
Count Derp <thecount@moehae.com>
2013-08-18 13:25:41 -0500

    forgot to add attributes/default.rb

---
+++ attributes/default.rb
@@ -0,0 +1,8 @@
+normal['openssh']['Port']                 = 1001
+normal['openssh']['Protocol']             = 2
+normal['openssh']['PermitRootLogin']      = 'no'
+normal['openssh']['LoginGraceTime']       = '5s'
+normal['openssh']['MaxAuthTries']         = 5
+normal['openssh']['MaxSession']           = 4
+normal['openssh']['AuthorizedKeyFile']    = '.ssh/authorized_keys'
+normal['openssh']['PermitEmptyPasswords'] = 'no'
