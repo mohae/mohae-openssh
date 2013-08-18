0ee808dff0bbe14749b5e4e6af35e8c308fdf299
Count Derp <thecount@moehae.com>
2013-08-18 13:25:24 -0500

    move settings to attributes and bump

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,12 +7,4 @@
 # All rights reserved - Do Not Redistribute
 #
 
-node.override['openssh']['Port']                 = 1001
-node.override['openssh']['Protocol']             = 2
-node.override['openssh']['PermitRootLogin']      = 'no'
-node.override['openssh']['LoginGraceTime']       = '30s'
-node.override['openssh']['MaxAuthTries']         = 5
-node.override['openssh']['MaxSession']           = 4
-node.override['openssh']['AuthorizedKeyFile']    = '.ssh/authorized_keys'
-node.override['openssh']['PermitEmptyPasswords'] = 'no'
 include_recipe "openssh"
