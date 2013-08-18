b6e000ec69acd8119686f9406c55b42ae335ee0d
Count Derp <thecount@moehae.com>
2013-08-18 13:59:39 -0500

    comment out ssh port change for vagrant purposes. this should be an environment or role thing

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 
-default['openssh']['server']['port']                 = '1001'
+#default['openssh']['server']['port']                 = '1001'
 default['openssh']['server']['protocol']             = '2'
 default['openssh']['server']['login_grace_time']     = '5s'
 default['openssh']['server']['permit_root_login']      = 'no'
